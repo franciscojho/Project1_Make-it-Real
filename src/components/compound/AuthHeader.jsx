2f85a8ee9f79ccc6cac56f1bc314524669f962ba
Francisco Huapaya <franciscojho@hotmail.com>
2022-02-20 18:15:06 -0500

    Fix for media queries prefix, not painting bg with designated colors

--- src/components/compound/AuthHeader.jsx
+++ src/components/compound/AuthHeader.jsx
@@ -1,8 +1,8 @@
 const AuthHeader = ({ children, loginTheme = true }) => {
     return (
         <section
-            className={`flex flex-col text-tertiary md:justify-start md:min-w-96 md:${
-                loginTheme ? 'bg-primary' : 'bg-secondary'
+            className={`flex flex-col text-tertiary md:justify-start md:min-w-96 ${
+                loginTheme ? 'md:bg-primary' : 'md:bg-secondary'
             }`}>
             <header className="flex flex-col pt-8 mr-auto ml-auto max-w-xl md:p-8">
                 {children}
