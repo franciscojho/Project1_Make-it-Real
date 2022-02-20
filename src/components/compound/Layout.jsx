f4b0b2ec9b9294fbb5ccf431f3f2ea69a3294326
Francisco Huapaya <franciscojho@hotmail.com>
2022-02-20 17:44:26 -0500

    Add tailwind styles to layout component

--- src/components/compound/Layout.jsx
+++ src/components/compound/Layout.jsx
@@ -1,11 +1,5 @@
-// import styles from './Styles.module.css'
-
 const Layout = ({ children, className = '' }) => {
-    return (
-        <div className={className} style={{ height: '100%', width: '100%' }}>
-            {children}
-        </div>
-    )
+    return <div className={`w-full h-full ${className}`}>{children}</div>
 }
 
 export default Layout
