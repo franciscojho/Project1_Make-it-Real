5f19c987be0f5307286db119544e93cca971599b
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-14 12:02:18 -0400

    Add global navbar and empty dashboard component in routes

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,8 +1,10 @@
 import { Navigate, Routes, Route, Outlet } from 'react-router-dom'
-import LoginPage from '../pages/client/LoginPage/LoginPage'
-import HomePage from '../pages/client/HomePage/HomePage'
-import RegisterPage from '../pages/client/RegisterPage/RegisterPage'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
+import Dashboard from '../pages/Dashboard/Dashboard'
 import { useAuthContext } from '../context/auth'
+import Navbar from './Navbar'
+import { Layout } from '../components'
 
 const PrivateWrapper = ({ auth: { token } }) => {
     return token ? <Outlet /> : <Navigate to="/login" />
@@ -11,16 +13,20 @@ const PrivateWrapper = ({ auth: { token } }) => {
 const Navigation = () => {
     const { state } = useAuthContext()
     return (
-        <Routes>
-            {state.token && (
-                <Route element={<PrivateWrapper auth={{ token: state.token }} />}>
-                    <Route path="/home" element={<HomePage />} />
-                </Route>
-            )}
-            {!state.token && <Route path="/login" element={<LoginPage />} />}
-            {!state.token && <Route path="/register" element={<RegisterPage />} />}
-            <Route path="*" element={<Navigate to={state.token ? '/home' : '/login'} />} />
-        </Routes>
+        <Layout className={`${state.token && 'grid grid-rows-6 grid-cols-10'}`}>
+            {state.token && <Navbar />}
+            <Routes>
+                {state.token && (
+                    <Route element={<PrivateWrapper auth={{ token: state.token }} />}>
+                        <Route path="/dashboard" state="testing" element={<Dashboard />} />
+                        <Route path="/account" element={<h1>CUENTA</h1>} />
+                    </Route>
+                )}
+                {!state.token && <Route path="/login" element={<LoginPage />} />}
+                {!state.token && <Route path="/register" element={<RegisterPage />} />}
+                <Route path="*" element={<Navigate to={state.token ? '/dashboard' : '/login'} />} />
+            </Routes>
+        </Layout>
     )
 }
 
