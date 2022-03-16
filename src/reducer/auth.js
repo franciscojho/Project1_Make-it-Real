51620904c6d3cb6c98f39a07b32bacc59cae76e7
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-16 13:35:56 -0400

    Add additonally actions and cases for user update

--- src/reducer/auth.js
+++ src/reducer/auth.js
@@ -1,15 +1,28 @@
 export const AUTH_LOGIN = 'AUTH_LOGIN'
+export const AUTH_LOGOUT = 'AUTH_LOGOUT'
 export const AUTH_REGISTER = 'AUTH_REGISTER'
-
-// const initialState = { token: null, user: {} }
+export const USER_UPDATE_REQUEST = 'USER_UPDATE_REQUEST'
+export const USER_UPDATE_SUCCESS = 'USER_UPDATE_SUCCESS'
+export const USER_LOADING = 'USER_LOADING'
 
 export const authReducer = (state, action) => {
     switch (action.type) {
         case AUTH_LOGIN:
             return { ...state, token: action.payload.token, user: action.payload.user }
 
+        case AUTH_LOGOUT:
+            return { ...state, token: null, user: null }
+
         case AUTH_REGISTER:
             return { ...state, token: action.payload.token, user: action.payload.user }
+
+        case USER_UPDATE_REQUEST:
+            return { ...state, isLoading: true }
+
+        case USER_UPDATE_SUCCESS:
+            return { ...state, user: action.payload, isLoading: false }
+
+        // case USER_ERROR_
 
         default:
             return { ...state }
