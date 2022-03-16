980f0fb681476dd10201a5c3d3390856327181b8
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-16 13:50:32 -0400

    Add work in progress view in dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { Layout } from '../../components'
+import workInProgress from '../../assets/work-in-progress.png'
 
 const Dashboard = () => {
     return (
-        <Layout className="row-span-full col-span-8 bg-quaternary">
-            <h1>Dashboard</h1>
+        <Layout className="flex flex-col items-center justify-center bg-quaternary row-span-full col-span-8">
+            <h1 className="p-4 text-center font-bold text-3xl">WORK IN PROGRESS</h1>
+            <img className="w-1/2" src={workInProgress} alt="Work in progress" />
         </Layout>
     )
 }
