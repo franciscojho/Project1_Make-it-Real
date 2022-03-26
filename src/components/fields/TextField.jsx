60a18eb418af09b371bff303f294625586adb2b5
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-26 11:29:26 -0400

    Add new component for text using formik

---
+++ src/components/fields/TextField.jsx
@@ -0,0 +1,18 @@
+import { ErrorMessage, useField } from 'formik'
+
+const TextField = ({ label, className, type, ...props }) => {
+    const [field] = useField(props)
+    return (
+        <div className="flex flex-col gap-y-2 w-full">
+            <label htmlFor={props.name}>{label}</label>
+            <input className={className} type={type || 'text'} {...field} {...props} />
+            <ErrorMessage
+                className="text-red-500 font-bold text-sm"
+                name={props.name}
+                component="span"
+            />
+        </div>
+    )
+}
+
+export default TextField
