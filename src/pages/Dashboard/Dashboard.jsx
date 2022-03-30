93a347beff3dc28df0986594c8437ad765a79df9
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-30 16:01:55 -0400

    Modify dashboard
    Import new components and add function handlSave

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,61 @@
-import { Layout } from '../../components'
-import workInProgress from '../../assets/work-in-progress.png'
+import { useState } from 'react'
+import AdsList from './AdsList'
+import CustomModal from './CustomModal'
+import { Button, Layout } from '../../components'
+import { createAd, updateAd } from '../../api'
+import { useAuthContext } from '../../context/auth'
+import { useAdContext } from '../../context/ad'
+import {
+    CREATE_AD_REQUEST,
+    CREATE_AD_SUCCESS,
+    UPDATE_AD_REQUEST,
+    UPDATE_AD_SUCCESS,
+} from '../../reducer/ad'
 
 const Dashboard = () => {
+    const { state: auth } = useAuthContext()
+    const { adDispatch } = useAdContext()
+
+    const [open, setOpen] = useState(false)
+    const [selectedAd, setSelectedAd] = useState({})
+
+    const handleSave = (values) => {
+        if (values && !selectedAd.id) {
+            adDispatch({ type: CREATE_AD_REQUEST })
+            createAd(auth.token, values).then((ad) =>
+                adDispatch({ type: CREATE_AD_SUCCESS, payload: ad })
+            )
+        }
+
+        if (values && selectedAd.id) {
+            adDispatch({ type: UPDATE_AD_REQUEST })
+            updateAd(auth.token, { ...values, id: selectedAd.id }).then((ad) =>
+                adDispatch({ type: UPDATE_AD_SUCCESS, payload: { selectedAd, ad } })
+            )
+        }
+        setOpen(false)
+        setSelectedAd({})
+    }
+
     return (
-        <Layout className="flex flex-col items-center justify-center bg-quaternary row-span-full col-span-8">
-            <h1 className="p-4 text-center font-bold text-3xl">WORK IN PROGRESS</h1>
-            <img className="w-1/2" src={workInProgress} alt="Work in progress" />
+        <Layout className="flex flex-col p-6 bg-quaternary row-span-full col-span-8 gap-6">
+            <h2 className="font-bold text-2xl">Configuración de anuncios de limpieza</h2>
+
+            <AdsList setOpen={setOpen} setSelectedAd={setSelectedAd} />
+
+            <CustomModal
+                handleSave={handleSave}
+                selectedAd={selectedAd}
+                setOpen={setOpen}
+                open={open}
+                setSelectedAd={setSelectedAd}
+            />
+
+            <Button
+                className="bg-primary w-full md:max-w-[200px] px-4 py-2 text-tertiary font-bold rounded"
+                onClick={() => setOpen(true)}>
+                Añadir Anuncio
+            </Button>
         </Layout>
     )
 }
