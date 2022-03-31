851a6bc9d40af2ecadc3f36881d657b506717890
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-31 16:11:25 -0400

    Create useForm
    This hook handles the form actions, create, delete, update

---
+++ src/hooks/useForm.js
@@ -0,0 +1,70 @@
+import { useAlert } from 'react-alert'
+import { createAd, deleteAd, updateAd } from '../api'
+import { useAdContext } from '../context/ad'
+import { useModalContext } from '../context/modal'
+import {
+    CREATE_AD_SUCCESS,
+    DELETE_AD_SUCCESS,
+    REQUEST_AD_API,
+    UPDATE_AD_SUCCESS,
+} from '../reducer/ad'
+
+const token = localStorage.getItem('token') || ''
+
+const useForm = () => {
+    const alert = useAlert()
+    const { handleModal } = useModalContext()
+    const { adState, adDispatch } = useAdContext()
+
+    const { isLoading } = adState
+
+    const handleCreateAd = (ad) => {
+        adDispatch({ type: REQUEST_AD_API })
+        setTimeout(() => {
+            createAd(token, ad)
+                .then((res) => {
+                    adDispatch({ type: CREATE_AD_SUCCESS, payload: res })
+                    alert.success('Anuncio creado con éxito')
+                })
+                .catch(() => {
+                    alert.error('Error al crear el anuncio')
+                })
+                .finally(() => {
+                    handleModal()
+                })
+        }, 2000)
+    }
+
+    const handleUpdateAd = (ad, id) => {
+        adDispatch({ type: REQUEST_AD_API })
+        setTimeout(() => {
+            updateAd(token, { ...ad, id })
+                .then((res) => {
+                    adDispatch({ type: UPDATE_AD_SUCCESS, payload: { id, ad: res } })
+                    alert.success('Anuncio actualizado con éxito')
+                })
+                .catch(() => {
+                    alert.error('Error al actualizar el anuncio')
+                })
+                .finally(() => {
+                    handleModal()
+                })
+        }, 2000)
+    }
+
+    const handleDeleteAd = (id) => {
+        adDispatch({ type: REQUEST_AD_API })
+        deleteAd(token, id)
+            .then((res) => {
+                adDispatch({ type: DELETE_AD_SUCCESS, payload: { id: res.id } })
+                alert.success('Anuncio eliminado con éxito')
+            })
+            .catch(() => {
+                alert.error('Error al borrar el anuncio')
+            })
+    }
+
+    return { handleCreateAd, handleUpdateAd, handleDeleteAd, isLoading }
+}
+
+export default useForm
