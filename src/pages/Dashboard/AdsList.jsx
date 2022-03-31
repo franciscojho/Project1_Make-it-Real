f20148d9767627bcead1aec213c7f4895a3442e2
Francisco Huapaya <franciscojho@hotmail.com>
2022-03-31 16:17:38 -0400

    Major changes
    Remove logic from component and encapsulate it in useForm hook

--- src/pages/Dashboard/AdsList.jsx
+++ src/pages/Dashboard/AdsList.jsx
@@ -1,49 +1,29 @@
 import { useEffect } from 'react'
-import { deleteAd, getAds } from '../../api'
+import { useAlert } from 'react-alert'
+import { getAds } from '../../api'
 import { useAdContext } from '../../context/ad'
-import { useAuthContext } from '../../context/auth'
-import {
-    DELETE_AD_REQUEST,
-    DELETE_AD_SUCCESS,
-    GET_ADS_REQUEST,
-    GET_ADS_SUCCESS,
-} from '../../reducer/ad'
+import { GET_ADS_SUCCESS, REQUEST_AD_API } from '../../reducer/ad'
 import AdCard from './AdCard'
 
-const AdsList = ({ setOpen, setSelectedAd }) => {
-    const { adState, adDispatch } = useAdContext()
-    const { state: auth } = useAuthContext()
+const token = localStorage.getItem('token') || ''
 
-    const { ads } = adState
+const AdsList = () => {
+    const alert = useAlert()
+    const {
+        adState: { ads },
+        adDispatch,
+    } = useAdContext()
 
     useEffect(() => {
-        adDispatch({ type: GET_ADS_REQUEST })
-        getAds(auth.token).then((res) => adDispatch({ type: GET_ADS_SUCCESS, payload: res }))
-    }, [adDispatch, auth.token])
-
-    const handleUpdate = (id) => {
-        ads.filter((ad) => ad.id === id).map((ad) => setSelectedAd(ad))
-        setOpen(true)
-    }
-
-    const handleDelete = (id) => {
-        adDispatch({ type: DELETE_AD_REQUEST })
-        deleteAd(auth.token, id).then((ad) =>
-            adDispatch({ type: DELETE_AD_SUCCESS, payload: { id: ad.id } })
-        )
-    }
+        adDispatch({ type: REQUEST_AD_API })
+        getAds(token)
+            .then((res) => adDispatch({ type: GET_ADS_SUCCESS, payload: res }))
+            .catch(() => alert('Error al obtener los anuncios'))
+    }, [adDispatch, alert])
 
     return (
         <div className="flex flex-col gap-2">
-            {!!ads.length &&
-                ads.map((ad) => (
-                    <AdCard
-                        key={ad.id}
-                        ad={ad}
-                        handleUpdate={handleUpdate}
-                        handleDelete={handleDelete}
-                    />
-                ))}
+            {!!ads.length && ads.map((ad) => <AdCard key={ad.id} ad={ad} />)}
             {!ads.length && <p>No hay anuncios</p>}
         </div>
     )
