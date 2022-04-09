7ba4ff8f14ce3105cf982a58d6976b190fda0c8f
Francisco Huapaya <franciscojho@hotmail.com>
2022-04-08 23:52:44 -0400

    Improve Textarea component and add unit testing

--- src/components/fields/TextareaField.jsx
+++ src/components/fields/TextareaField.jsx
@@ -1,14 +1,14 @@
 import { ErrorMessage, useField } from 'formik'
 
-const TextareaField = ({ label, className, type, ...props }) => {
+const TextareaField = ({ label, className, ...props }) => {
     const [field] = useField(props)
     return (
         <div className="flex flex-col gap-y-2 w-full">
-            <label htmlFor={props.name}>{label}</label>
-            <textarea className={className} {...field} {...props} />
+            {label && <label htmlFor={field.name}>{label}</label>}
+            <textarea className={className} {...props} {...field} />
             <ErrorMessage
                 className="text-red-500 font-bold text-sm"
-                name={props.name}
+                name={field.name}
                 component="span"
             />
         </div>
