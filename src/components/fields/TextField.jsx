9cd63ede16fecc1c7aacf6b68aabd772b7946bb8
Francisco Huapaya <franciscojho@hotmail.com>
2022-04-08 23:53:00 -0400

    Improve TextField component and add unit testing

--- src/components/fields/TextField.jsx
+++ src/components/fields/TextField.jsx
@@ -4,11 +4,11 @@ const TextField = ({ label, className, type, ...props }) => {
     const [field] = useField(props)
     return (
         <div className="flex flex-col gap-y-2 w-full">
-            <label htmlFor={props.name}>{label}</label>
-            <input className={className} type={type || 'text'} {...field} {...props} />
+            {label && <label htmlFor={field.name}>{label}</label>}
+            <input className={className} {...props} {...field} type="text" />
             <ErrorMessage
                 className="text-red-500 font-bold text-sm"
-                name={props.name}
+                name={field.name}
                 component="span"
             />
         </div>
