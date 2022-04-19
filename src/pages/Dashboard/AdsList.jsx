596020284edc9441a88f65d867a21e988d12ced4
Francisco Huapaya <franciscojho@hotmail.com>
2022-04-18 21:50:14 -0400

    Send query string to getAds api

--- src/pages/Dashboard/AdsList.jsx
+++ src/pages/Dashboard/AdsList.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react'
 import { useAlert } from 'react-alert'
+import { useSearchParams } from 'react-router-dom'
 import { getAds } from '../../api'
 import { useAdContext } from '../../context/ad'
 import { GET_ADS_SUCCESS, REQUEST_AD_API, REQUEST_AD_ERROR } from '../../reducer/ad'
@@ -13,17 +15,19 @@ const AdsList = () => {
         adDispatch,
     } = useAdContext()
 
+    const [searchParams] = useSearchParams()
+
     useEffect(() => {
         if (token) {
             adDispatch({ type: REQUEST_AD_API })
-            getAds(token)
+            getAds(token, searchParams)
                 .then((res) => adDispatch({ type: GET_ADS_SUCCESS, payload: res }))
                 .catch((error) => {
                     adDispatch({ type: REQUEST_AD_ERROR })
                     alert.error(error.message)
                 })
         }
-    }, [adDispatch, alert, token])
+    }, [adDispatch, searchParams])
 
     return (
         <div className="flex flex-col gap-2">
