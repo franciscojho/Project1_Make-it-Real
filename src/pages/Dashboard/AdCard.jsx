b9314c4a345340208d76391c64865f8ef96d99b3
Francisco Huapaya <franciscojho@hotmail.com>
2022-04-18 21:47:29 -0400

    Major change
    Apply useModal hook and add a view button

--- src/pages/Dashboard/AdCard.jsx
+++ src/pages/Dashboard/AdCard.jsx
@@ -1,37 +1,60 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrash, faEye } from '@fortawesome/free-solid-svg-icons'
 import useForm from '../../hooks/useForm'
-import { useModalContext } from '../../context/modal'
+import { Button } from '../../components'
+import useModal from '../../hooks/useModal'
 
 const AdCard = ({ ad }) => {
-    const { handleModal } = useModalContext()
+    const { openModal } = useModal()
     const { handleDeleteAd } = useForm()
 
-    const { name, status, id } = ad
+    const handleView = () => {
+        openModal(ad, 'view')
+    }
+
+    const handleEdit = () => {
+        openModal(ad, 'edit')
+    }
 
     return (
-        <div className="flex items-center justify-between gap-6 bg-tertiary h-24 md:w-3/5 px-6 py-2 rounded-lg shadow-sm">
-            <span className="flex gap-10">
-                <p className="font-bold">{name}</p>
-                <p
-                    className={`${
-                        status === 'active' ? 'text-green-600' : 'text-red-500'
-                    } font-semibold`}>
-                    {status}
-                </p>
-            </span>
-            <span className="flex gap-10">
-                <FontAwesomeIcon
-                    icon={faEdit}
-                    className="cursor-pointer hover:text-secondary"
-                    onClick={() => handleModal(ad)}
-                />
-                <FontAwesomeIcon
-                    icon={faTrash}
-                    className="cursor-pointer text-red-500"
-                    onClick={() => handleDeleteAd(id)}
-                />
-            </span>
+        <div className="bg-tertiary flex flex-wrap h-20 items-center py-2 px-6 rounded-lg shadow">
+            <p className="flex-1 font-bold min-w-fit">{ad.name}</p>
+            <div className="flex flex-1 flex-wrap justify-between min-w-fit">
+                <div className="text-xs">
+                    <p className="hidden md:block">Creado en: {ad.createdAt}</p>
+                    <p className="hidden md:block">Expira en: {ad.expirationDate}</p>
+                </div>
+                <div className="flex gap-4">
+                    <p
+                        className={`${
+                            !ad.expired ? 'text-green-600' : 'text-red-500'
+                        } font-semibold`}>
+                        {!ad.expired ? 'Activo' : 'Expirado'}
+                    </p>
+                    <Button onClick={handleView}>
+                        <FontAwesomeIcon
+                            icon={faEye}
+                            className="cursor-pointer text-red-gray hover:opacity-70"
+                        />
+                    </Button>
+                    <Button disabled={ad.expired} onClick={handleEdit}>
+                        <FontAwesomeIcon
+                            icon={faEdit}
+                            className={`${
+                                !ad.expired
+                                    ? 'cursor-pointer text-secondary hover:opacity-70'
+                                    : 'cursor-not-allowed text-gray-400'
+                            }`}
+                        />
+                    </Button>
+                    <Button onClick={() => handleDeleteAd(ad.id)}>
+                        <FontAwesomeIcon
+                            icon={faTrash}
+                            className="cursor-pointer text-red-500 hover:opacity-70"
+                        />
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
